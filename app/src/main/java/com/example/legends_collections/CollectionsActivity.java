850db5110bfee73a4d8a812038b1aee7f85eaa01
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-11-29 09:26:17 +0300

    made the design for the site to be pushed

---
+++ app/src/main/java/com/example/legends_collections/CollectionsActivity.java
@@ -0,0 +1,12 @@
+package com.example.legends_collections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CollectionsActivity extends AppCompatActivity {
+
+    private String[] clothes = new String[] {"shirt", "Jeans",
+            "T-shirts", "Trousers", "socks", "dunguress",
+            "Clacks", "Air Force", "suits", "",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
+}
