aa988a57a39bb9b72662153d25e036d2c2a24a1d
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-12-02 00:25:15 +0300

    the form fragment not working as expected

--- app/src/main/java/com/example/legends_collections/ClothesActivity.java
+++ app/src/main/java/com/example/legends_collections/ClothesActivity.java
@@ -2,6 +2,9 @@ package com.example.legends_collections;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +12,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ClothesActivity extends AppCompatActivity {
+public class ClothesActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.name123)
     TextView mUserName;
+    @BindView(R.id.clothName)
+    EditText mCloth;
+    @BindView(R.id.buttonAdd)
+    Button addCloth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,10 @@ public class ClothesActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String username = intent.getStringExtra("username");
         mUserName.setText(username);
+        addCloth.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View log) {}
 
 }
