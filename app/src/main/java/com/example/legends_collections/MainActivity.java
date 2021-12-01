154e9cfad4713c658fd1dc9ea5ea8f9a7089ba7e
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-12-01 23:35:54 +0300

    view binded the login page with the the content that will be recieved.

--- app/src/main/java/com/example/legends_collections/MainActivity.java
+++ app/src/main/java/com/example/legends_collections/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.legends_collections;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.button2)
+    Button mGreet;
+    @BindView(R.id.name1)
+    EditText mUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mGreet.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View log) {
+        if (log == mGreet){
+            String username = mUserName.getText().toString();
+            if (username.isEmpty()){
+                Toast.makeText(MainActivity.this, "Please input Username", Toast.LENGTH_SHORT).show();
+            }else{
+                Intent intent = new Intent(MainActivity.this, ClothesActivity.class);
+                intent.putExtra("username",username);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Hooray your in!!!", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
     }
 }
